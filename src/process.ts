a748b24c68fee0794d8f87d0e16b7ed2e557e4c1
Jake Shadle <jake.shadle@embark-studios.com>
2020-05-05 17:02:05 +0200

    Moar debugging

--- src/process.ts
+++ src/process.ts
@@ -58,6 +58,8 @@ export async function process_event(ctx: Context, octo: Octokit, requires_descri
 
             const author_id: number = pr.user.id;
 
+            core.debug(`PR author id is ${author_id}`);
+
             // If any of the reviews are not APPROVED, we mark the PR as still
             // waiting on reviews
             if (reviews.data.length > 0) {
@@ -71,6 +73,7 @@ export async function process_event(ctx: Context, octo: Octokit, requires_descri
 
                     // Ignore review comments from the author
                     if (review.user.id === author_id) {
+                        core.debug(`Skipping ${review.state} of PR author`);
                         continue;
                     }
 
